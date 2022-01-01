92966095f19dbfae87bbe2b5b78d352dfb1bb4be
test <test@test.com>
2022-01-01 14:00:14 +0530

    Signup user graphql mutation.

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { Mutation } from 'react-apollo'
+import { SIGNUP_USER } from './../../queries'
 
 function Signup() {
 
@@ -27,53 +29,79 @@ function Signup() {
   }
 
   // form submit event handler
-  const handleSubmit = (e) => {
+  const handleSubmit = (e, signupUser) => {
     e.preventDefault()
 
-    console.log(formData)
+    //console.log(formData)
+
+    // call the graphql mutation function
+    signupUser().then(data => {
+      console.log(data);
+
+      //clear form fields
+      setFormData({ ...schema })
+    }).catch(error => {
+      //clear form fields
+      setFormData({ ...schema })
+    })
+
   }
 
   return (
     <div className='App'>
       <h1 className='App'>Signup</h1>
-      <form className='form' onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder='Username'
-          className=''
-          value={formData.username}
-          onChange={handleInputChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder='Email'
-          className=''
-          value={formData.email}
-          onChange={handleInputChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder='Password'
-          className=''
-          value={formData.password}
-          onChange={handleInputChange}
-        />
-        <input
-          type="password"
-          name="password2"
-          placeholder='Confirm Password'
-          className=''
-          value={formData.password2}
-          onChange={handleInputChange}
-        />
-        <button
-          type="submit"
-          className='button-primary'
-        >Signup</button>
-      </form>
+      <Mutation mutation={SIGNUP_USER} variables={{
+        username: formData.username,
+        email: formData.email,
+        password: formData.password
+      }}>
+        {
+          (signupUser, { data, loading, error }) => {
+            return (
+              <form className='form' onSubmit={(e) => handleSubmit(e, signupUser)}>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder='Username'
+                  className=''
+                  value={formData.username}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder='Email'
+                  className=''
+                  value={formData.email}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder='Password'
+                  className=''
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="password"
+                  name="password2"
+                  placeholder='Confirm Password'
+                  className=''
+                  value={formData.password2}
+                  onChange={handleInputChange}
+                />
+                <button
+                  type="submit"
+                  className='button-primary'
+                >Signup</button>
+
+                {error && <p>{error.message}</p>}
+              </form>
+            )
+          }
+        }
+      </Mutation>
     </div>
   )
 }
--- src/queries/index.js
+++ src/queries/index.js
@@ -1,5 +1,6 @@
 import { gql } from 'apollo-boost'
 
+/* Recipe Queries Start */
 export const GET_ALL_RECIPES = gql`
   query{
     getAllRecipes {
@@ -12,3 +13,32 @@ export const GET_ALL_RECIPES = gql`
     }
   }
 `
+
+/* Recipe Queries Ends */
+
+/* Recipe Mutation Starts */
+
+
+/* Recipe Mutation Ends */
+
+/* ---------------------- */
+
+/* User Queries Start */
+
+/* User Queries Ends */
+
+/* User Mutation Starts */
+export const SIGNUP_USER = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    signupUser(
+      username: $username,
+      email: $email,
+      password: $password,
+    ) {
+      token
+    }
+  }
+`
+
+
+/* User Mutation Ends */
